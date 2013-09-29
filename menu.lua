9fdbbd52650bb56f449d9350c4e5824379c89aea
Nixola <niki.or97@yahoo.it>
2013-09-29 15:37:21 +0200

    Create menu.lua
    UGLY main menu. White or red text over a black background. Ugly.

---
+++ menu.lua
@@ -0,0 +1,41 @@
+local menu = {}
+local lg = love.graphics
+
+menu.current = 1
+
+menu.options = {
+
+	{name = 'Play', func = function() state = require 'map' end},
+	{name = 'Settings', func = function() state = require 'settings' end},
+	{name = 'Quit', func = love.event.quit}}
+
+
+menu.update = function(self, dt)
+
+	--maybe not even needed
+	
+end
+
+
+menu.draw = function(self)
+
+	for i, v in pairs(self.options) do
+	
+		local _ = self.current and lg.setColor {255,0,0} or lg.setColor {255,255,255}
+		
+		lg.printf(v.name, 0, 256+32*i, 800, 'center')
+		
+	end
+	
+end
+
+
+menu.keypressed = function(self, k, u)
+
+	current = k == 'up' and math.max(current-1, 1) or (k == 'down' and math.min(current+1, #self.options) or current)
+	
+	if k == 'return' then self.options[self.current].func() end
+	
+end
+
+return menu
